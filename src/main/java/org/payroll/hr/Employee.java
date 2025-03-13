9a06dbf5956d65cb25899d2c359c838c0bcecfa2
bairigao <122509008+bairigao@users.noreply.github.com>
2025-03-13 14:18:29 +1000

    Create Employee class and Payable interface

---
+++ src/main/java/org/payroll/hr/Employee.java
@@ -0,0 +1,51 @@
+package org.payroll.hr;
+
+public abstract class Employee implements Payable{
+
+    private String name;
+    private Double payRate;
+    private final int EMPLOYEE_ID;
+    private Address address;
+    private static int nextID = 1000;
+    protected static final double STARTING_PAY_RATE = 7.75;
+
+    public Employee(String name) {
+        this.name = name;
+        payRate = STARTING_PAY_RATE;
+        EMPLOYEE_ID = getNextID();
+    }
+
+    public Employee(String name, Double payRate) {
+        this.name = name;
+        this.payRate = payRate;
+        EMPLOYEE_ID = getEmployeeID();
+    }
+
+    public String getName() { return this.name; }
+
+    public int getEmployeeID() { return this.EMPLOYEE_ID; }
+
+    public double getPayRate() { return this.payRate; }
+
+    public void displayAddress() { Address.getAddress(); }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setName(String name) { this.name = name; }
+
+    public void setPayRate(double payRate) { this.payRate = payRate; }
+
+    public static int getNextID() {
+        int id = nextID;
+        nextID++;
+        return id;
+    }
+
+    @Override
+    public String toString(){
+        return "Employee ID - " + this.EMPLOYEE_ID + "\nName - " +this.name
+                + "\n" + this.address.toString();
+    }
+}
---
+++ src/main/java/org/payroll/hr/Payable.java
@@ -0,0 +1,7 @@
+package org.payroll.hr;
+
+public interface Payable {
+    double calculatePay();
+
+    String generatePayStub(String payDate);
+}
